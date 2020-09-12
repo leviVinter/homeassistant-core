aa32105f8888317ba010c461834de1981d104b76
Chris Talkington <chris@talkingtontech.com>
2020-09-12 13:27:41 -0500

    Update test_init.py

--- tests/components/canary/test_init.py
+++ tests/components/canary/test_init.py
@@ -1,13 +1,13 @@
 """The tests for the Canary component."""
-from homeassistant.components.canary import DOMAIN
-from homeassistant.setup import async_setup_component
+from unittest.mock import patch
 
-from tests.async_mock import patch
+from homeassistant.components.canary import DOMAIN
+from homeassistant.setup import setup_component
 
 
 async def test_setup_with_valid_config(hass, canary) -> None:
     """Test setup with valid YAML."""
-    assert await async_setup_component(hass, "persistent_notification", {})
+    assert setup_component(hass, "persistent_notification", {})
     config = {DOMAIN: {"username": "foo@bar.org", "password": "bar"}}
 
     with patch(
@@ -20,5 +20,5 @@ async def test_setup_with_valid_config(hass, canary) -> None:
         "homeassistant.components.canary.sensor.setup_platform",
         return_value=True,
     ):
-        assert await async_setup_component(hass, DOMAIN, config)
-        await hass.async_block_till_done()
+        assert setup_component(hass, DOMAIN, config)
+        hass.block_till_done()
